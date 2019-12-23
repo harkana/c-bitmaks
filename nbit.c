75d38e3a5a6a79ad017c6641264da8755086d988
Thomas SIMOES <simoes_t@etna-alternance.net>
2019-12-23 16:21:14 +0100

    bitmask to nb, nb to bitmask

--- nbit.c
+++ nbit.c
@@ -23,16 +23,43 @@ char	*nb_to_bitmask(int n, int length)
   return (str);
 }
 
+int	power(int n, int f)
+{
+  if (f > 0)
+    {
+      return (n * power(n, f - 1));
+    }
+  return (1);
+}
+
+int	bitmask_to_nb(char *str, int length)
+{
+  int	n;
+  int	i;
+
+  n = i = 0;
+  while (str[i])
+    {
+      n += ((str[i] - 48) * power(2, length - i - 1));
+      i++;
+    }
+  return (n);
+}
+
 int	main(void)
 {
   int	a;
   char	*str;
+  int	nb;
 
-  a = 1;
+  a = 53;
   if ((str = nb_to_bitmask(a, 8)) == NULL)
     {
       return (EXIT_FAILURE);
     }
-  printf("%s\n", str);      
+  printf("%s\n", str);
+  nb = bitmask_to_nb(str, 8);
+  printf("nb: %d\n", nb);
+  printf("2 puissance 4 : %d\n", power(2, 4));
   return (EXIT_SUCCESS);
 }
